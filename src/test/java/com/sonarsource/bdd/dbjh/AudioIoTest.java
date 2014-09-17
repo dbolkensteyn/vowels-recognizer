5d71defbb55e8edde72d020f9f5d0568dd6b040d
Julien HENRY <julien.henry@sonarsource.com>
2014-09-17 16:48:27 +0200

    Load and play a wav file

---
+++ src/test/java/com/sonarsource/bdd/dbjh/AudioIoTest.java
@@ -0,0 +1,16 @@
+package com.sonarsource.bdd.dbjh;
+
+import com.sonarsource.bdd.dbjh.AudioIo.AudioSignal;
+
+import org.junit.Test;
+
+
+public class AudioIoTest {
+
+  @Test
+  public void loadWav() throws Exception {
+    AudioSignal wavFile = AudioIo.loadWavFile("a_jh_bruyant.wav");
+    
+    AudioIo.play(wavFile);
+  }
+}
