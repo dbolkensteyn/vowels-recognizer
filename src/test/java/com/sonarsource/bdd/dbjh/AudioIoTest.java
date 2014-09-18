4cd8060c4bfb9e72b2c7e87379b60c2a22fe55d6
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-09-18 11:15:21 +0200

    Fix a quality flaw

--- src/test/java/com/sonarsource/bdd/dbjh/AudioIoTest.java
+++ src/test/java/com/sonarsource/bdd/dbjh/AudioIoTest.java
@@ -2,13 +2,13 @@ package com.sonarsource.bdd.dbjh;
 
 import org.junit.Test;
 
-
 public class AudioIoTest {
 
   @Test
   public void loadWav() throws Exception {
     AudioSignal wavFile = AudioIo.loadWavFile("a_jh_bruyant.wav");
-    
+
     AudioIo.play(wavFile);
   }
+
 }
